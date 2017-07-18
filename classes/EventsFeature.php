73083cc9e300ad01cb6b0c31abe0910ca9ce605c
Samuel Heer <samuel.heer@diging.de>
2017-07-18 22:19:05 +0200

    make it possible to change feature settings for each calendar in one eventlist

--- classes/EventsFeature.php
+++ classes/EventsFeature.php
@@ -17,14 +17,22 @@
 
    public function getFeaturedEvents($arrEvents, $arrCalendars, $intStart, $intEnd, $objModule) {
 
-     // Only show events accordingly to moule featured settings
+    //Erstelle ein Array, bei dem die Calender Id (event-pid) als key gespeichert wird
+    $objModuleCalendarFeatured = deserialize($objModule->cal_calendar_featured);
+    if(!empty($objModuleCalendarFeatured)){
+      foreach ($objModuleCalendarFeatured as $val){
+        $arrCalendarFeatured[$val['calendar']] = $val;
+      }  
+    }
+
+    // Only show events accordingly to module featured settings
  		foreach ($arrEvents as $key=>$days)
  		{
  			foreach ($days as $day=>$events)
  			{
  				foreach ($events as $arrCol=>$event)
  				{
-          switch ($objModule->events_featured) {
+          switch ($arrCalendarFeatured[$event['pid']]['featured']) {
             case 'featured_events':
               if ($event[featured] != 1) {
                 // nicht gefeatured Events löschen
--- dca/tl_module.php
+++ dca/tl_module.php
@@ -12,12 +12,19 @@
  */
 
 
+
  // Anpassung der Palette
- $GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist'] = str_replace(
-     'perPage;',
-     'perPage,events_featured;',
+$GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist'] = str_replace(
+     //'perPage',
+     //'perPage,events_featured,cal_featured',
+     'cal_calendar',
+     'cal_calendar_featured',
      $GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist']
  );
+
+$GLOBALS['TL_DCA']['tl_module']['config']['onsubmit_callback'][] = array(
+	'tl_module_calendar_feature','saveCalFeatured'
+	);
 
 // Feld hinzufügen
 $GLOBALS['TL_DCA']['tl_module']['fields']['events_featured'] = array
@@ -28,6 +35,69 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['events_featured'] = array
 	'inputType'               => 'select',
 	'options'                 => array('all_events', 'featured_events', 'unfeatured_events'),
 	'reference'               => &$GLOBALS['TL_LANG']['tl_module'],
-	'eval'                    => array('tl_class'=>'w50'),
+	'eval'                    => array('tl_class'=>'w50 long'),
 	'sql'                     => "varchar(18) NOT NULL default ''"
 );
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['cal_calendar_featured'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['cal_calendar'],
+	'exclude'                 => true,
+	'inputType'               => 'multiColumnWizard',
+	'eval'                    => array(
+			'columnFields' => array
+				(
+				'calendar' => array
+					(
+						'inputType' => 'select',
+						'options_callback' => array('tl_module_calendar','getCalendars'),
+						'eval' => array
+							(
+								'style' => 'width:250px'
+							)
+					),
+				'featured' => array
+					(
+						'inputType' => 'select',
+						'options' 	=> array('all_events', 'featured_events', 'unfeatured_events'),
+						'default'	=> 'all_items',
+						'reference' => &$GLOBALS['TL_LANG']['tl_module'],
+						'eval' => array
+							(
+								'style' => 'width:250px'
+							)
+					)
+				)
+		),
+	'sql'	=> "blob NULL"
+);
+
+class tl_module_calendar_feature extends Backend{
+
+	public function saveCalFeatured(DataContainer $dc){
+
+		// Return if there is no active record (override all)
+		if (!$dc->activeRecord)
+		{
+			return;
+		}
+
+		$calendar_feature = deserialize($dc->activeRecord->cal_calendar_featured);
+
+		$arrSet['cal_calendar']=array();
+			if(isset($calendar_feature)){
+				foreach($calendar_feature as $key=>$entry){
+				if(in_array($entry['calendar'],$arrSet['cal_calendar'])){
+					unset($calendar_feature[$key]);
+				}
+				else{
+					$arrSet['cal_calendar'][] = $entry['calendar'];	
+				}
+			}
+		}
+		
+		$arrSet['cal_calendar_featured'] = $calendar_feature;
+
+		$this->Database->prepare("UPDATE tl_module %s WHERE id=?")->set($arrSet)->execute($dc->id);
+	}
+}
